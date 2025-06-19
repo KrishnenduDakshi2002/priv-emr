5a352f83518e0921015c7d849d2f02245fefc7dd
Debayan Pradhan <pradhandebayan@gmail.com>
2025-06-19 08:14:43 +0530

    [UPDATE] raviger routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
-import { Route, Routes } from "react-router-dom";
-
-import IndexPage from "@/pages/index";
-import DocsPage from "@/pages/docs";
-import PricingPage from "@/pages/pricing";
-import BlogPage from "@/pages/blog";
-import AboutPage from "@/pages/about";
+import { useRoutes } from 'raviger';
+import { routes } from './routes/routes';
 
 function App() {
+  const route = useRoutes(routes);
+
   return (
-    <Routes>
-      <Route element={<IndexPage />} path="/" />
-      <Route element={<DocsPage />} path="/docs" />
-      <Route element={<PricingPage />} path="/pricing" />
-      <Route element={<BlogPage />} path="/blog" />
-      <Route element={<AboutPage />} path="/about" />
-    </Routes>
-  );
+    <>
+      {routes}
+    </>
+  )
 }
 
 export default App;
